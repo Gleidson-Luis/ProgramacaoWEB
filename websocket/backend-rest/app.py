22111482efa3a2823be511a8b5d1d7e277fa41b6
Gleidson Luis <gleidson.luis@academico.ifrn.edu.br>
2026-01-22 21:29:26 -0300

    backend-rest3

--- websocket/backend-rest/app.py
+++ websocket/backend-rest/app.py
@@ -7,5 +7,5 @@ app = Flask(__name__)
 def getTemperatura():
     return jsonify({"valor": random.randint(20,50)})
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8001, debug=True)
